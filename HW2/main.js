4e2cfa41868556f58dff011e77de4b26a82e62a5
bxxmi <bmi6638@gmail.com>
2021-08-08 13:33:10 +0900

    refactor: for loop를 고차함수 filter로 변경함

--- HW2/main.js
+++ HW2/main.js
@@ -16,8 +16,24 @@ const data = [
 const line = prompt('최대 금액을 입력해주세요.');
 const amount = +line;
 
+// 아래에 getItemByAmount 함수를 작성하세요.
+const getItemByAmount = (amount) => {
+    // 사용자의 입력 유효값 범위 지정: 숫자가 아닌 경우, 0인 경우 null
+    if(amount === false || amount === 0) {
+        return null;
+    }
+
+    // filter, sort 함수 적용하여 입력한 값보다 저렴한 상품들의 리스트를 오름차순으로 정렬함
+    const canBuyItems = data.filter(items => items.price < amount).
+    sort((a, b) => b.price - a.price);
+
+    // 만약, canBuyItems 변수에 값이 존재한다면 맨 처음 값을 반환하고 
+    // 없다면 null 출력해라
+    return canBuyItems ? canBuyItems[0] : null
+};
+
 // 주어진 금액으로 살 수 있는 가장 비싼 상품을 구함
-const item = getItemByAmount(data, amount);
+const item = getItemByAmount(amount);
 
 const msg = item ? 
     `${amount}원으로 살 수 있는 가장 비싼 상품은 [${item.name}]이고, 가격은 ${item.price}원입니다.` : 
@@ -25,27 +41,3 @@ const msg = item ?
 
 // 결과 출력
 alert(msg);
-
-// 아래에 getItemByAmount 함수를 작성하세요.
-const getItemByAmount = (data, amount) => {
-    // 사용자의 입력 유효값 범위 지정: 숫자가 아닌 경우, 0인 경우 null
-    if(amount === false || amount === 0) {
-        return null;
-    }
-
-    // 살 수 있는 상품을 저장하기 위한 변수
-    const index;
-
-    for(let i=0; i<data.length; i++) {
-        // 조건: 사용자가 입력한 금액보다 상품의 가격이 더 비싼 경우
-        if(amount < data[i].price) {
-            // continue: 해당 항목을 제외하고 그 다음 항목과 비교하기 위함
-            continue;
-        } 
-            // 그 외의 경우 사용자가 상품을 살 수 있기 때문에 해당 상품의 인덱스 번호가 저장됨
-            index = i;
-    }
-    // 상품이 존재한다면 해당 상품의 데이터를 출력하고, 그게 아니라면 null을 출력
-    return index !== null ? data[index] : null;
-};
-
